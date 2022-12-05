f1c4ed196f0c48125da1304bab4bdfa34c610a7b
박영익 <81441317+naldPark@users.noreply.github.com>
2022-12-05 18:33:20 +0900

    Update CommonController.java

--- src/main/java/me/nald/blog/controller/CommonController.java
+++ src/main/java/me/nald/blog/controller/CommonController.java
@@ -1,33 +1,30 @@
 package me.nald.blog.controller;
 
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import me.nald.blog.annotation.WithoutJwtCallable;
-import me.nald.blog.data.dto.AccountDto;
 import me.nald.blog.data.model.ContactRequest;
-import me.nald.blog.data.persistence.entity.Account;
-import me.nald.blog.service.AccountService;
 import me.nald.blog.service.CommonService;
-import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
-import java.util.List;
 import java.util.concurrent.Callable;
-
-import static java.util.Arrays.asList;
 
 @AllArgsConstructor
 @RestController
-@RequestMapping("common")
+@RequestMapping("/common")
+@Slf4j
 public class CommonController {
 
     private final CommonService commonService;
 
 
     @WithoutJwtCallable
-    @PostMapping(value = "/sendMail", consumes = MediaType.APPLICATION_JSON_VALUE)
-    public Callable<Object> checkUser(@RequestBody ContactRequest contactRequest) {
+    @PostMapping("/sendMail")
+    public Callable<Object> sendMail(@RequestBody ContactRequest contactRequest) {
+        System.out.println("메일보내기");
         return () -> commonService.sendMail(contactRequest);
     }
 
