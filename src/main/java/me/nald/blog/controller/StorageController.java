f58c92050840743750eb133686c4d688eaff1295
박영익 <81441317+naldPark@users.noreply.github.com>
2023-02-06 16:02:16 +0900

    1

--- src/main/java/me/nald/blog/controller/StorageController.java
+++ src/main/java/me/nald/blog/controller/StorageController.java
@@ -46,147 +46,24 @@ public class StorageController {
     private final StorageService storageService;
 
     @WithoutJwtCallable
-    @PostMapping("/download")
-    public Callable<Object> postWorkspacesBackupDownloads(HttpServletRequest request) {
-        return () -> {
-            Path zipFilePath = storageService.postWorkspaceDownloads();
-            try {
-                Resource resource = new FileSystemResource(zipFilePath) {
-                    @Override
-                    public InputStream getInputStream() throws IOException {
-                        return new FileInputStream(zipFilePath.toFile()) {
-                            @Override
-                            public void close() throws IOException {
-                                super.close();
-//                                Files.delete(zipFilePath);
-                            }
-                        };
-                    }
-                };
-                MediaType mediaType = MediaTypeFactory.getMediaType(resource).orElse(MediaType.APPLICATION_OCTET_STREAM);
-                HttpHeaders headers = new HttpHeaders();
-                headers.setContentType(mediaType);
-                headers.setContentDisposition(ContentDisposition.builder("attachment").filename(resource.getFilename()).build());
-                return ResponseEntity.ok().headers(headers).body(resource);
-            } catch (Exception e) {
-                return ResponseEntity.status(HttpStatus.CONFLICT).build();
-            }
-        };
+    @GetMapping("/playVideo/{videoId}")
+    public Callable<Object> playVideo(@PathVariable Long videoId) {
+        return () -> storageService.playVideo(videoId);
     }
 
-
-    @WithoutJwtCallable
-    @GetMapping(value = "/streamingVideo")
-    public void viewMp4Stream(HttpServletRequest request, HttpServletResponse response, int videoId) throws IOException {
-        System.out.println("어섭쇼video2");
-        Map<Integer, String> videoPath = new HashMap<>();
-        videoPath.put(1, "sample.mp4");
-        videoPath.put(2, "HarryPotterAndTheSorcerersStone.mp4");
-
-        File file = new File(Constants.STORAGE_FILE_PATH + videoPath.get(videoId));
-        RandomAccessFile randomFile = new RandomAccessFile(file, "r");
-        long rangeStart = 0; //요청 범위의 시작 위치
-        long rangeEnd = 0; //요청 범위의 끝 위치
-        boolean isPart = false; //부분 요청일 경우 true, 전체 요청의 경우 false
-        try { //동영상 파일 크기
-            long movieSize = randomFile.length(); //스트림 요청 범위, request의 헤더에서 range를 읽는다.
-
-            String range = request.getHeader("range");
-            if (range != null) {
-                if (range.endsWith("-")) {
-                    range = range + (movieSize - 1);
-                }
-                int idxm = range.trim().indexOf("-");
-                rangeStart = Long.parseLong(range.substring(6, idxm));
-                rangeEnd = Long.parseLong(range.substring(idxm + 1));
-                if (rangeEnd >= movieSize) {
-                    rangeEnd = movieSize - 1;
-                }
-                if (rangeStart > 0) {
-                    isPart = true;
-                }
-            } else {
-                rangeStart = 0;
-                rangeEnd = movieSize - 1;
-            }
-            long partSize = rangeEnd - rangeStart + 1;
-            response.reset();
-            response.setStatus(isPart ? 206 : 200);
-            response.setContentType("video/mp4");
-            response.setHeader("Content-Range", "bytes " + rangeStart + "-" + rangeEnd + "/" + movieSize);
-            response.setHeader("Accept-Ranges", "bytes");
-            response.setHeader("Content-Length", "" + partSize);
-            response.setHeader("totalsize", "" + movieSize);
-            OutputStream out = response.getOutputStream();
-            randomFile.seek(rangeStart);
-            int bufferSize = 8 * 1024;
-            byte[] buf = new byte[bufferSize];
-            do {
-                int block = partSize > bufferSize ? bufferSize : (int) partSize;
-                int len = randomFile.read(buf, 0, block);
-                out.write(buf, 0, len);
-                partSize -= block;
-            } while (partSize > 0);
-        } catch (IOException e) {
-        } finally {
-            randomFile.close();
-        }
-    }
 
     @WithoutJwtCallable
     @GetMapping("/hls/{fileName}/{fileName}.m3u8")
     public ResponseEntity<Resource> videoHlsM3U8(@PathVariable String fileName) {
 
-         ResponseEntity<Resource> test = storageService.videoHlsM3U8(fileName);
-
-        System.out.println("리턴직전 컨트롤러"+test);
-
-        return test;
-
-//        return new ResponseEntity<Resource>(resource, headers, HttpStatus.OK);
+        return storageService.videoHlsM3U8(fileName);
     }
-
 
     @WithoutJwtCallable
     @GetMapping("/hls/{fileName}/{tsName}.ts")
     public ResponseEntity<Resource> videoHlsTs(@PathVariable String fileName, @PathVariable String tsName) {
-
-        ResponseEntity<Resource> test = storageService.videoHlsTs(fileName, tsName);
-
-        System.out.println("리턴직전 ts 컨트롤러"+test);
-
-        return test;
+        return  storageService.videoHlsTs(fileName, tsName);
    }
-
-
-    @WithoutJwtCallable
-    @GetMapping("/hls/test.ts")
-    public ResponseEntity<Resource> videoHlsTstest() {
-
-        ResponseEntity<Resource> test = storageService.videoHlsTstest();
-
-        System.out.println("리턴직전 ts 컨트롤러"+test);
-
-        return test;
-    }
-
-    @WithoutJwtCallable
-    @GetMapping("/hls2/{fileName}/{tsName}.ts")
-    public ResponseEntity<Resource> videoHlsTstest2(@PathVariable String fileName, @PathVariable String tsName) {
-        String fileFullPath = storageService.videoHlsTstest2(fileName, tsName);
-
-        Resource resource = new FileSystemResource(fileFullPath);
-
-        System.out.println("FileSystemResource 요청완료");
-        HttpHeaders headers = new HttpHeaders();
-        headers.set(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + tsName + ".ts");
-        headers.setContentType(MediaType.parseMediaType(MediaType.APPLICATION_OCTET_STREAM_VALUE));
-
-        System.out.println("리턴할게");
-        return new ResponseEntity<Resource>(resource, headers, HttpStatus.OK);
-    }
-    
-
 
     @WithoutJwtCallable
     @GetMapping("/videoHls/{fileName}")
@@ -212,13 +89,5 @@ public class StorageController {
 //            return responseObject;
 //        };
 //    }
-
-    @WithoutJwtCallable
-    @GetMapping("/playVideo/{videoId}")
-    public Callable<Object> playVideo(@PathVariable Long videoId) {
-        return () -> storageService.playVideo(videoId);
-    }
-
-
 
 }
--- src/main/java/me/nald/blog/service/StorageService.java
+++ src/main/java/me/nald/blog/service/StorageService.java
@@ -2,15 +2,9 @@ package me.nald.blog.service;
 
 import lombok.RequiredArgsConstructor;
 import me.nald.blog.config.BlogProperties;
-import me.nald.blog.data.dto.AccountDto;
 import me.nald.blog.data.dto.StorageDto;
 import me.nald.blog.data.persistence.entity.Storage;
-import me.nald.blog.repository.AccountRepository;
 import me.nald.blog.repository.StorageRepository;
-import me.nald.blog.response.CommonResponse;
-import me.nald.blog.response.Response;
-import me.nald.blog.util.FileUtils;
-import me.nald.blog.util.Util;
 import net.bramp.ffmpeg.FFmpeg;
 import net.bramp.ffmpeg.FFmpegExecutor;
 import net.bramp.ffmpeg.FFprobe;
@@ -46,74 +40,56 @@ public class StorageService {
         this.blogProperties = blogProperties;
     }
 
-
-    public Path postWorkspaceDownloads() {
-        Path zipFilePath = Paths.get(blogProperties.getCommonPath()+"/movie/hls/sample").resolve("sample0." + "ts");
-
-        return zipFilePath;
+    public Map<String, Object> playVideo(Long videoId) {
+        HashMap<String, Object> map = new HashMap<>();
+        Storage storage = storageRepository.getById(videoId);
+        StorageDto.StorageInfo res = new StorageDto.StorageInfo(storage);
+        map.put("statusCode", 200);
+        map.put("data", res);
+        return map;
     }
 
     public Boolean videoHls(String movieName) {
         Boolean result = false;
-        FFmpeg ffmpeg = null;
-        FFprobe ffprobe = null;
-        String movieDir = blogProperties.getCommonPath() + "/movie";
-        String fileName = FilenameUtils.getBaseName(movieName);
-        String inputPath = movieDir + "/upload/";
-        String HlsPath = movieDir + "/hls/" + fileName + "/";
-
         try {
-            String osName = System.getProperty("os.name");
+            String movieDir = blogProperties.getCommonPath() + "/movie";
+            String fileName = FilenameUtils.getBaseName(movieName);
+            String inputPath = movieDir + "/upload/";
+            String HlsPath = movieDir + "/hls/" + fileName + "/";
+
             File folder = new File(HlsPath);
             if (!folder.exists()) {
-                folder.mkdir(); //폴더 생성합니다.
+                folder.mkdir();
             }
-//            if (osName.toLowerCase().contains("unix") || osName.toLowerCase().contains("linux")) {
-                ffmpeg = new FFmpeg(blogProperties.getFfmpegPath() + "/ffmpeg");
-                ffprobe = new FFprobe(blogProperties.getFfmpegPath() + "/ffprobe");
-//            } else {
-//                ffmpeg = new FFmpeg(blogProperties.getFfmpegPath() + "/ffmpeg.exe");
-//                ffprobe = new FFprobe(blogProperties.getFfmpegPath() + "/ffprobe.exe");
-//            }
+            FFmpeg ffmpeg = new FFmpeg(blogProperties.getFfmpegPath() + "/ffmpeg");
+            FFprobe ffprobe = new FFprobe(blogProperties.getFfmpegPath() + "/ffprobe");
+
+            FFmpegBuilder builder = new FFmpegBuilder()
+                    .overrideOutputFiles(true)
+                    .setInput(inputPath + movieName)
+                    .addOutput(HlsPath + fileName + ".m3u8")
+                    .addExtraArgs("-profile:v", "baseline")
+                    .addExtraArgs("-level", "3.0")
+                    .addExtraArgs("-start_number", "0")
+                    .addExtraArgs("-hls_time", "10")
+                    .addExtraArgs("-hls_list_size", "0")
+                    .addExtraArgs("-f", "hls")
+                    .addExtraArgs("-safe", "0")
+                    .addExtraArgs("-preset", "ultrafast")
+                    .setVideoResolution(1920, 1080)
+                    .setStrict(FFmpegBuilder.Strict.EXPERIMENTAL)
+                    .done();
+
+            builder.setVerbosity(FFmpegBuilder.Verbosity.INFO);
+            FFmpegExecutor executor = new FFmpegExecutor(ffmpeg, ffprobe);
+            FFmpegJob job = executor.createJob(builder);
+            job.run();
+            if (job.getState() == FFmpegJob.State.FINISHED) {
+                result = true;
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-        FFmpegBuilder builder = new FFmpegBuilder()
-                .overrideOutputFiles(true)
-                .setInput(inputPath + movieName)
-                .addOutput(HlsPath + fileName + ".m3u8")
-//                .addExtraArgs("-acodec", "copy")
-//                .addExtraArgs("-vcodec", "copy")
-                .addExtraArgs("-profile:v", "baseline")
-                .addExtraArgs("-level", "3.0")
-                .addExtraArgs("-start_number", "0")
-                .addExtraArgs("-hls_time", "10")
-                .addExtraArgs("-hls_list_size", "0")
-                .addExtraArgs("-f", "hls")
-//                .setVideoBitRate(1)
-                .addExtraArgs("-safe", "0")
-                .addExtraArgs("-preset", "ultrafast")
-                .setVideoResolution(1920, 1080)
-                .setStrict(FFmpegBuilder.Strict.EXPERIMENTAL)
-                .done();
-
-        builder.setVerbosity(FFmpegBuilder.Verbosity.INFO);
-        System.out.println("익스큐트전");
-
-        FFmpegExecutor executor = new FFmpegExecutor(ffmpeg, ffprobe);
-        System.out.println("job생성");
-        FFmpegJob job = executor.createJob(builder);
-
-        System.out.println("job런");
-        job.run();
-        System.out.println("job런완료");
-        if (job.getState() == FFmpegJob.State.FINISHED) {
-            System.out.println("리절트 트루");
-            result = true;
-        }
-
-
         return result;
     }
 
@@ -123,11 +99,8 @@ public class StorageService {
         String fileName = FilenameUtils.getBaseName(movieName);
         String HlsPath = movieDir + "/hls/" + fileName + "/";
         String fileFullPath = HlsPath + fileName + ".m3u8";
-        System.out.println("FileSystemResource ㅇ요청할게");
-
         Path filePath = Paths.get(fileFullPath);
-
-        Resource resource = new FileSystemResource(filePath){
+        Resource resource = new FileSystemResource(filePath) {
             @Override
             public InputStream getInputStream() throws IOException {
                 return new FileInputStream(filePath.toFile()) {
@@ -139,16 +112,10 @@ public class StorageService {
                 };
             }
         };
-
-        System.out.println("FileSystemResource 요청완료");
         HttpHeaders headers = new HttpHeaders();
         MediaType mediaType = MediaTypeFactory.getMediaType(resource).orElse(MediaType.APPLICATION_OCTET_STREAM);
         headers.setContentDisposition(ContentDisposition.builder("attachment").filename(resource.getFilename()).build());
-//        headers.set(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + fileName + ".m3u8");
         headers.setContentType(mediaType);
-//        headers.setContentType(MediaType.parseMediaType("application/vnd.apple.mpegurl"));
-
-        System.out.println("리턴할게");
         return ResponseEntity.ok().headers(headers).body(resource);
 
     }
@@ -158,97 +125,24 @@ public class StorageService {
         String fileName = FilenameUtils.getBaseName(movieName);
         String HlsPath = movieDir + "/hls/" + fileName + "/";
         String fileFullPath = HlsPath + tsName + ".ts";
-        System.out.println("FileSystemResource ㅇ요청할게");
 
         Path filePath = Paths.get(fileFullPath);
-        Resource resource = new FileSystemResource(filePath){
+        Resource resource = new FileSystemResource(filePath) {
             @Override
             public InputStream getInputStream() throws IOException {
                 return new FileInputStream(filePath.toFile()) {
                     @Override
                     public void close() throws IOException {
                         super.close();
-//                                Files.delete(zipFilePath);
                     }
                 };
             }
         };
-        System.out.println("FileSystemResource 요청완료");
         HttpHeaders headers = new HttpHeaders();
         MediaType mediaType = MediaTypeFactory.getMediaType(resource).orElse(MediaType.APPLICATION_OCTET_STREAM);
         headers.setContentDisposition(ContentDisposition.builder("attachment").filename(resource.getFilename()).build());
-//        headers.set(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + fileName + ".m3u8");
         headers.setContentType(mediaType);
-//        headers.setContentType(MediaType.parseMediaType("application/vnd.apple.mpegurl"));
-
-        System.out.println("리턴할게");
         return ResponseEntity.ok().headers(headers).body(resource);
-
-    }
-
-    public ResponseEntity<Resource> videoHlsTstest() {
-        String fileFullPath = "test.ts";
-        System.out.println("FileSystemResource ㅇ요청할게");
-        Resource resource = new FileSystemResource(fileFullPath);
-
-        System.out.println("FileSystemResource 요청완료");
-        HttpHeaders headers = new HttpHeaders();
-        headers.set(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + "test.ts");
-        headers.setContentType(MediaType.parseMediaType(MediaType.APPLICATION_OCTET_STREAM_VALUE));
-
-        System.out.println("리턴할게");
-        return new ResponseEntity<Resource>(resource, headers, HttpStatus.OK);
-
-    }
-
-    public String videoHlsTstest2(String movieName, String tsName) {
-        String movieDir = blogProperties.getCommonPath() + "/movie";
-        String fileName = FilenameUtils.getBaseName(movieName);
-        String HlsPath = movieDir + "/hls/" + fileName + "/";
-        String fileFullPath = HlsPath + tsName + ".ts";
-        System.out.println("리턴할게");
-        return fileFullPath;
-
-    }
-
-
-//    public void uploadVideo(List<MultipartFile> files, String userId, Long noticeId, int groupId) {
-//        String noticeFilePath = Constants.FILE_MANAGER_PATH_PREFIX + Constants.NOTICE_FILE_PATH;
-//        String folderPath = noticeFilePath + "/" + noticeId;
-//        try {
-//            for (int i = 0; i < files.size(); ++i) {
-//                MultipartFile currentFile = files.get(i);
-//                FileUtils.createDirectoriesIfNotExists(folderPath);
-//                String fileName = currentFile.getOriginalFilename();
-//                String ext = fileName.substring(fileName.lastIndexOf("."));
-//                String filePath = folderPath + "/" + System.currentTimeMillis() + (int) (Math.random() * 1000000) + i + ext;
-//                CreateAdminNoticeFile createAdminNoticeFile = new CreateAdminNoticeFile(fileName, filePath, currentFile.getSize(), userId, noticeId, groupId);
-//                InputStream readStream = currentFile.getInputStream();
-//                byte[] readBytes = new byte[4096];
-//                OutputStream writeStream = Files.newOutputStream(Paths.get(createAdminNoticeFile.getFileSrc()));
-//                while(readStream.read(readBytes) > 0) {
-//                    writeStream.write(readBytes);
-//                }
-//                writeStream.close();
-//                adminNoticeDAO.createNoticeFile(createAdminNoticeFile);
-//            }
-//        } catch (IOException e) {
-//            log.error("write file failed {}", e);
-//        }
-//    }
-
-    public Map<String, Object> playVideo(Long videoId) {
-
-        HashMap<String, Object> map = new HashMap<>();
-        Storage storage = storageRepository.getById(videoId);
-        StorageDto.StorageInfo res = new StorageDto.StorageInfo(storage);
-        System.out.println(storage);
-
-        map.put("statusCode", 200);
-        ;
-        map.put("data", res);
-
-        return map;
 
     }
 
