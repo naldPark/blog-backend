a338c294e4a822ee1519daf9c2ff21a948844770
Nald <nald873@gmaill.com>
2023-12-05 14:07:36 +0900

    Update CommonService.java

--- src/main/java/me/nald/blog/service/CommonService.java
+++ src/main/java/me/nald/blog/service/CommonService.java
@@ -92,7 +92,7 @@ public class CommonService {
     public Response.CommonRes getBlogList() {
 
         List<String> inputList = new ArrayList<>();
-        inputList.add("{src: 'gitbook.png', header: 'CURRENT BLOG', title: 'GITBOOK', text: 'blog',  href: 'https://blog.nald.me'}");
+        inputList.add("{src: 'gitbook.png', header: 'CURRENT BLOG', title: 'GITBOOK', text: 'blog',  href: 'https://daylog.nald.me'}");
         inputList.add("{src: 'github.png', header: 'Configuration Tool', title: 'GITHUB',  text: 'github',  href: 'https://github.com/naldPark'}");
         inputList.add("{src: 'naverblog.png', header: 'BLOG (Deprecated)', title: 'NAVER BLOG',  text: 'naver',  href: 'https://blog.naver.com/8734747'}");
 
